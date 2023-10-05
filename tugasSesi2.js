eb61ecc55d4519e690d0d476dd91df93abf9d5ed
Dimas Arbi Ardian <dimas.arbi_ti22@nusaputra.ac.id>
2023-10-05 20:42:09 +0700

    Create tugasSesi2.js

---
+++ tugasSesi2.js
@@ -0,0 +1,9 @@
+var firstName = "NamaDepan";
+var lastName = "NamaBelakang";
+var age = 25; // Ganti dengan umur Anda
+var isMarried = false; // Ganti dengan true jika Anda sudah menikah atau false jika belum
+
+console.log("Nama Depan: " + firstName);
+console.log("Nama Belakang: " + lastName);
+console.log("Umur: " + age + " tahun");
+console.log("Status Pernikahan: " + (isMarried ? "Sudah Menikah" : "Belum Menikah"));
