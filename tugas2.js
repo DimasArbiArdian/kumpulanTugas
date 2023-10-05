6ab009ee82cced075d7757b47a9db0f53de3ccc4
Dimas Arbi Ardian <114743682+DimasArbiArdian@users.noreply.github.com>
2023-10-05 21:38:14 +0700

    Create tugas2.js

---
+++ tugas2.js
@@ -0,0 +1,9 @@
+const profile = {
+    firstName: "Dimas",
+    lastName : "Arbi Ardian",
+    age: 20,
+    isMarried: false
+    
+};
+const {firstName, lastName, age, isMarried} = profile;
+console.log (`Hallo Nama Saya ${firstName} ${lastName} Umur Saya ${age} isMarried ${isMarried}`)
