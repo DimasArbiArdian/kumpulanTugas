6d8789a7e070fc425c89d81e1250da5398c823db
Dimas Arbi Ardian <114743682+DimasArbiArdian@users.noreply.github.com>
2023-10-20 18:43:56 +0700

    Add files via upload

---
+++ tugasSesi3.js
@@ -0,0 +1,25 @@
+// Objek restaurant
+const restaurantObj = {
+  name: "The Flavour",
+  city: "Sukabumi",
+  favoriteDrink: "Tomato Juice",
+  favoriteFood: "Balado Tofu",
+  isVegan: true
+};
+
+// Array restaurant
+const restaurantArr = [
+  "The Heaven",
+  "Sukabumi",
+  "Wine",
+  "Steak",
+  false
+];
+
+// Cetak objek restaurant
+console.log("Objek restaurant:");
+console.log(restaurantObj);
+
+// Cetak array restaurant
+console.log("Array restaurant:");
+console.log(restaurantArr);
